9ea272f09b8b6cb880a1aced352e59bf94e2b199
anshuvarma <anshu.v1011@gmail.com>
2024-07-01 22:12:56 +0530

    completed experience page

--- lib/pages/experience_page.dart
+++ lib/pages/experience_page.dart
@@ -22,6 +22,14 @@ class ExperiencePage extends StatelessWidget {
           "PEOPLE FIRST",
           style: TextStyle(color: Colors.white),
         ),
+        const SizedBox(
+          height: 5.0,
+        ),
+
+        const Text(
+          "Driving the creation of People First, a cutting-edge mobile application developed with Flutter, has significantly enhanced interactions for over 550,000 users in real time. By integrating 22 essential HR processes—including hiring, onboarding, engagement, and offboarding—the application streamlines workflows and boosts productivity. Its user-centric design not only improves overall efficiency but also ensures a seamless and engaging experience, fostering a more connected and productive workforce",
+          style: TextStyle(color: Colors.white),
+        ),
         // Add more skillscard as needed
       ],
 
@@ -31,6 +39,13 @@ class ExperiencePage extends StatelessWidget {
           "JIO FACE DASHBOARD",
           style: TextStyle(color: Colors.white),
         ),
+        const SizedBox(
+          height: 5.0,
+        ),
+        const Text(
+          "Developed MongoDB views with sophisticated lookups, grouping, projection, and matching that resulted in a 35% increase in data filtering efficiency. 50% less manual ETL work was required when using Airbyte to integrate MongoDB as a source and PostgreSQL as a destination. created an interactive, dynamic Power BI dashboard that improved data presentation by 40% and used DAX queries to get accurate attendance data. Oversaw gradual data refreshes for in-the-moment database changes.",
+          style: TextStyle(color: Colors.white),
+        ),
         // Add more skillscard as needed
       ],
 
@@ -38,6 +53,13 @@ class ExperiencePage extends StatelessWidget {
       [
         const Text(
           "THE SPARKS FOUNDATION",
+          style: TextStyle(color: Colors.white),
+        ),
+        const SizedBox(
+          height: 5.0,
+        ),
+        const Text(
+          "During my internship, I utilized Python to implement unsupervised machine learning techniques on the 'Iris' dataset. By applying KMeans clustering and employing the elbow method, I identified the optimal number of clusters. Visualizing these clusters not only facilitated the segmentation of Iris-setosa, Iris-versicolour, and Iris-virginica species based on their characteristics but also demonstrated my proficiency in data analysis and interpretation using machine learning algorithms.",
           style: TextStyle(color: Colors.white),
         ),
         // Add more skillscard as needed
